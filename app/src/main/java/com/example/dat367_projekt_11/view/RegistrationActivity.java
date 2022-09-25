6c60626777958960cd5b4efd5bd485ff7324f094
Hulling <hullingk@student.chalmers.se>
2022-09-25 20:27:42 +0200

    updatetest@

--- app/src/main/java/com/example/dat367_projekt_11/view/RegistrationActivity.java
+++ app/src/main/java/com/example/dat367_projekt_11/view/RegistrationActivity.java
@@ -1,25 +1,16 @@
 package com.example.dat367_projekt_11.view;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.dat367_projekt_11.R;
 import com.example.dat367_projekt_11.databinding.ActivityRegistrationBinding;
 import com.example.dat367_projekt_11.viewModels.RegistrationViewModel;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText emailTextView, passwordTextView;
-    private Button btn;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,11 +20,5 @@ public class RegistrationActivity extends AppCompatActivity {
         activityRegistrationBinding.setViewModelRegistration(new RegistrationViewModel());
         activityRegistrationBinding.executePendingBindings();
 
-    }
-
-    @BindingAdapter({"toastMessage"})
-    public static void runMe(View view, String message) {
-        if (message != null)
-            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 }

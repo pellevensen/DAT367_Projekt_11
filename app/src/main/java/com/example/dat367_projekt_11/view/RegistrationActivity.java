54f30d0eadac99b60faa3ff59da2617c7cfac995
Hulling <hullingk@student.chalmers.se>
2022-09-26 10:44:07 +0200

    config

--- app/src/main/java/com/example/dat367_projekt_11/view/RegistrationActivity.java
+++ app/src/main/java/com/example/dat367_projekt_11/view/RegistrationActivity.java
@@ -3,11 +3,6 @@ package com.example.dat367_projekt_11.view;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.example.dat367_projekt_11.R;
-import com.example.dat367_projekt_11.databinding.ActivityRegistrationBinding;
-import com.example.dat367_projekt_11.viewModels.RegistrationViewModel;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -16,9 +11,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        ActivityRegistrationBinding activityRegistrationBinding = DataBindingUtil.setContentView(this, R.layout.activity_registration);
+        /*ActivityRegistrationBinding activityRegistrationBinding = DataBindingUtil.setContentView(this, R.layout.activity_registration);
         activityRegistrationBinding.setViewModelRegistration(new RegistrationViewModel());
-        activityRegistrationBinding.executePendingBindings();
+        activityRegistrationBinding.executePendingBindings();*/
 
     }
 }

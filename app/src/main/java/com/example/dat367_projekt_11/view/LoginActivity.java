2099902a17dd7f30e60e9ec1ca5c6cc9669ba63f
Hulling <hullingk@student.chalmers.se>
2022-09-27 10:50:42 +0200

    config av two way data binding

--- app/src/main/java/com/example/dat367_projekt_11/view/LoginActivity.java
+++ app/src/main/java/com/example/dat367_projekt_11/view/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.dat367_projekt_11.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,9 @@ import com.example.dat367_projekt_11.viewModels.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button registerBtn;
+    private Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,6 +26,26 @@ public class LoginActivity extends AppCompatActivity {
         ActivityLoginBinding activityLoginBinding= DataBindingUtil.setContentView(this, R.layout.activity_login);
         activityLoginBinding.setLoginViewModel(new LoginViewModel());
         activityLoginBinding.executePendingBindings();
+
+        registerBtn = findViewById(R.id.createHousehold);
+        loginBtn = findViewById(R.id.login);
+
+        // Set on Click Listener on Sign-in button
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
 
 
     }

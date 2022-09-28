0ebe14ede3517d08235a2ffaf3abd9bde196867e
paob02 <93664147+paob02@users.noreply.github.com>
2022-09-28 14:59:00 +0200

    Försöker koppla min leaderboard fragment till bottom_nav_menu

--- app/src/main/java/com/example/dat367_projekt_11/view/MainActivity.java
+++ app/src/main/java/com/example/dat367_projekt_11/view/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.dat367_projekt_11.view;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 
 import com.example.dat367_projekt_11.R;
 import com.example.dat367_projekt_11.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,16 +17,45 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
 
+    BottomNavigationView bottomNavigationView;
+    ScoreboardFragment scoreboardFragment = new ScoreboardFragment();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_donechores, R.id.navigation_leaderboard, R.id.navigation_profile)
                 .build();
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,scoreboardFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item){
+                switch (item.getItemId()){
+                    /*case R.id.navigation_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,);
+                        return true;
+                    case R.id.navigation_donechores:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,);
+                        return true;*/
+                    case R.id.navigation_leaderboard:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,scoreboardFragment);
+                        return true;
+                    /*case R.id.navigation_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,);
+                        return true;*/
+                }
+                return false;
+            }
+        });
     }
 
-    }
+}
 
    /* @Override
     protected void onCreate(Bundle savedInstanceState) {

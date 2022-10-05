c916316969b1b0d5ca8c30ab04677eedc5f91993
Malin Kihlström <93664192+malinkih@users.noreply.github.com>
2022-10-05 11:09:26 +0200

    JObbat på att kunna byta vy från mainpage till create chore

--- app/src/main/java/com/example/dat367_projekt_11/view/MainPageView.java
+++ app/src/main/java/com/example/dat367_projekt_11/view/MainPageView.java
@@ -4,30 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.dat367_projekt_11.R;
 import com.example.dat367_projekt_11.databinding.FragmentMainpageBinding;
 import com.example.dat367_projekt_11.databinding.FragmentMainpageBinding;
 import com.example.dat367_projekt_11.viewModels.MainPageViewModel;
 
 public class MainPageView extends Fragment {
-
+private Button createButton;
 private FragmentMainpageBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        MainPageViewModel notificationsViewModel =
-                new ViewModelProvider(this, (ViewModelProvider.Factory) new ViewModelProvider.NewInstanceFactory()).get(MainPageViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_chore_page, container, false);
 
-    binding = FragmentMainpageBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
-
-        final TextView textView = binding.appTitle;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        createButton = view.findViewById(R.id.signInBtn);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.nav_host_fragment_activity_main); //TODO ändra detta så det blir rätt frgment
+            }
+        });
+        return view;
     }
 
 @Override
--- app/src/main/java/com/example/dat367_projekt_11/viewModels/MainPageViewModel.java
+++ app/src/main/java/com/example/dat367_projekt_11/viewModels/MainPageViewModel.java
@@ -4,18 +4,23 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.dat367_projekt_11.models.Household;
+
 public class MainPageViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
+    //private ChoreList chorelist;
     public MainPageViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue(""/*chorelist */);
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+
 
 
     }

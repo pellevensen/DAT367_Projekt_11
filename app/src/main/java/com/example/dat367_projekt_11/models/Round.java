0e4b0b93cf7120294dc1450c77dd6d6f1e1d8418
Malin Kihlström <93664192+malinkih@users.noreply.github.com>
2022-10-10 09:41:15 +0200

    Har gjort en timer och en timetask som har satt en nytt event en gång i veckan. Just nu säger den bara hej när timern går ut men jag ska nu försöka göra Round till en observable som notifyar listeners när timer går ut. Dock vet jag inte om detta är rätt sätt att göra det på

--- app/src/main/java/com/example/dat367_projekt_11/models/Round.java
+++ app/src/main/java/com/example/dat367_projekt_11/models/Round.java
@@ -1,25 +1,27 @@
 package com.example.dat367_projekt_11.models;
+/*
+HAR TAGIT HJÄLP AV https://www.baeldung.com/java-timer-and-timertask
+för att förstå timer, timerTask och treads
+ */
 
 
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Round {
-    Household household;
-    List<Profile> profiles;
+public class Round{
 
-    public Round(Household household) {
-        this.household = household;
-        profiles = household.getProfileList();
+    public void example(){
+        TimerTask roundTask = new TimerTask() {
+            public void run() {
+                System.out.println("Hej");
+            }
+        };
+        Timer timer = new Timer("Timer");
+        long delay = 1000L;
+        long period = 1000L * 60L * 60L * 24L *7L;
+        timer.scheduleAtFixedRate(roundTask,delay,period);
 
-
-    }
-
-    public void getWinner(){
-
-    }
-    public void resetPoints(){
-        for(Profile profile : profiles){
-            profile.resetScore();
         }
+
     }
-}
+

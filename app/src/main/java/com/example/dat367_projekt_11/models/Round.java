0d7af9a8138075d3175565ad7b511261341904c6
Malin Kihlström <93664192+malinkih@users.noreply.github.com>
2022-10-11 09:37:35 +0200

    Har skrivit in lite fler metoder i rätt ordning i round, men är osäker på om arbetet ska ligga i round eller om den ska agera obervable och att arbetet göra i houshold/scoreboard. Men det blir väl lika många dependencies bara åt andra hållet?

--- app/src/main/java/com/example/dat367_projekt_11/models/Round.java
+++ app/src/main/java/com/example/dat367_projekt_11/models/Round.java
@@ -10,33 +10,52 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Round{
-    Household household; //vet inte om  jag får tag i rätt household
+    Household household; //TODO hämta rätt household från firebase
     List<RoundListener> roundListeners;
 
-    public void example(){
+    public void onTimesUp() throws InterruptedException{
         TimerTask roundTask = new TimerTask() {
             public void run() {
-                notifySubscribers();//TODO determine winner and display on popup
-                for(Profile profile : household.getProfileList()){
-                    profile.resetScore();
-                }
+                determineWinner();
+                notifySubscribers();//TODO display on popup
+                resetScores();
+                resetChoreList();
             }
         };
         Timer timer = new Timer("Timer");
-        long delay = 1000L;
+        long delay = 10000L;
         long period = 1000L * 60L * 60L * 24L *7L;
         timer.scheduleAtFixedRate(roundTask,delay,period);
 
         }
 
     public void notifySubscribers(){
-        for(RoundListener roundListsner: roundListeners){
-            roundListsner.update();
+        for(RoundListener roundListener: roundListeners){
+            roundListener.update();
         }
     }
     public void subscribe(RoundListener listener){
         roundListeners.add(listener);
 
+    }
+    void determineWinner(){
+        /*
+        Unsure if this should be here or if this work should be in
+         household or scoreboard instead of delegating the work to them?
+         */
+
+    }
+
+    void resetScores(){     //again, unsure whether this should be here or in household
+        for(Profile profile : household.getProfileList()){
+            profile.resetScore();
+        }
+    }
+    void resetChoreList(){  //same with this method
+        for(Chore chore : household.getHouseholdChores()){
+            //TODO find a way to delete all chores from a household
+
+        }
     }
 
     }
